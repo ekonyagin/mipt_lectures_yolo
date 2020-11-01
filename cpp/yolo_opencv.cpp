8d944aae76eb89903e52b54c722b3229ad573055
egor <egorkonyagin@yandex.ru>
2020-11-01 13:56:50 +0300

    eliminated default NMS, added lightweight NMS

--- cpp/yolo_opencv.cpp
+++ cpp/yolo_opencv.cpp
@@ -4,6 +4,7 @@
 #include <opencv2/dnn.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
+#include <stdio.h>
 
 #ifdef CV_CXX11
 #include <mutex>
@@ -27,6 +28,8 @@ void postprocess(Mat& frame, const std::vector<Mat>& out, Net& net, int backend)
 void drawPred(int classId, float conf, int left, int top, int right, int bottom, Mat& frame);
 
 void callback(int pos, void* userdata);
+
+bool AddPoint(const Point& point, const std::vector<Point>& points);
 
 #ifdef CV_CXX11
 template <typename T>
@@ -296,50 +299,19 @@ inline void preprocess(const Mat& frame, Net& net, Size inpSize, float scale,
 
 void postprocess(Mat& frame, const std::vector<Mat>& outs, Net& net, int backend)
 {
+    int coords[2] = {0,0};
+
     static std::vector<int> outLayers = net.getUnconnectedOutLayers();
     static std::string outLayerType = net.getLayer(outLayers[0])->type;
-
+    printf("%s\n", "hello from postprocess 1\n");
     std::vector<int> classIds;
     std::vector<float> confidences;
     std::vector<Rect> boxes;
-    if (outLayerType == "DetectionOutput")
+    std::vector<Point> centers;
+    
+    if (outLayerType == "Region")
     {
-        // Network produces output blob with a shape 1x1xNx7 where N is a number of
-        // detections and an every detection is a vector of values
-        // [batchId, classId, confidence, left, top, right, bottom]
-        CV_Assert(outs.size() > 0);
-        for (size_t k = 0; k < outs.size(); k++)
-        {
-            float* data = (float*)outs[k].data;
-            for (size_t i = 0; i < outs[k].total(); i += 7)
-            {
-                float confidence = data[i + 2];
-                if (confidence > confThreshold)
-                {
-                    int left   = (int)data[i + 3];
-                    int top    = (int)data[i + 4];
-                    int right  = (int)data[i + 5];
-                    int bottom = (int)data[i + 6];
-                    int width  = right - left + 1;
-                    int height = bottom - top + 1;
-                    if (width <= 2 || height <= 2)
-                    {
-                        left   = (int)(data[i + 3] * frame.cols);
-                        top    = (int)(data[i + 4] * frame.rows);
-                        right  = (int)(data[i + 5] * frame.cols);
-                        bottom = (int)(data[i + 6] * frame.rows);
-                        width  = right - left + 1;
-                        height = bottom - top + 1;
-                    }
-                    classIds.push_back((int)(data[i + 1]) - 1);  // Skip 0th background class id.
-                    boxes.push_back(Rect(left, top, width, height));
-                    confidences.push_back(confidence);
-                }
-            }
-        }
-    }
-    else if (outLayerType == "Region")
-    {
+        printf("%s\n", "hello from postprocess 2\n");
         for (size_t i = 0; i < outs.size(); ++i)
         {
             // Network produces output blob with a shape NxC where N is a number of
@@ -348,69 +320,42 @@ void postprocess(Mat& frame, const std::vector<Mat>& outs, Net& net, int backend
             float* data = (float*)outs[i].data;
             for (int j = 0; j < outs[i].rows; ++j, data += outs[i].cols)
             {
+                
                 Mat scores = outs[i].row(j).colRange(5, outs[i].cols);
                 Point classIdPoint;
                 double confidence;
                 minMaxLoc(scores, 0, &confidence, 0, &classIdPoint);
-                if (confidence > confThreshold)
-                {
-                    int centerX = (int)(data[0] * frame.cols);
-                    int centerY = (int)(data[1] * frame.rows);
-                    int width = (int)(data[2] * frame.cols);
-                    int height = (int)(data[3] * frame.rows);
-                    int left = centerX - width / 2;
-                    int top = centerY - height / 2;
+                if (classIdPoint.x == 0){
+                    if (confidence > confThreshold)
+                    {
+                        int centerX = (int)(data[0] * frame.cols);
+                        int centerY = (int)(data[1] * frame.rows);
+                        int width = (int)(data[2] * frame.cols);
+                        int height = (int)(data[3] * frame.rows);
+                        int left = centerX - width / 2;
+                        int top = centerY - height / 2;
 
-                    classIds.push_back(classIdPoint.x);
-                    confidences.push_back((float)confidence);
-                    boxes.push_back(Rect(left, top, width, height));
+                        
+                        if (AddPoint(Point(centerX, centerY), centers)){
+                            centers.push_back(Point(centerX, centerY));
+                            classIds.push_back(classIdPoint.x);
+                            confidences.push_back((float)confidence);
+                            boxes.push_back(Rect(left, top, width, height));
+                        }
+                        
+                        //break;
+                    }
+
                 }
+                
             }
         }
     }
+
     else
         CV_Error(Error::StsNotImplemented, "Unknown output layer type: " + outLayerType);
-
-    // NMS is used inside Region layer only on DNN_BACKEND_OPENCV for another backends we need NMS in sample
-    // or NMS is required if number of outputs > 1
-    if (outLayers.size() > 1 || (outLayerType == "Region" && backend != DNN_BACKEND_OPENCV))
-    {
-        std::map<int, std::vector<size_t> > class2indices;
-        for (size_t i = 0; i < classIds.size(); i++)
-        {
-            if (confidences[i] >= confThreshold)
-            {
-                class2indices[classIds[i]].push_back(i);
-            }
-        }
-        std::vector<Rect> nmsBoxes;
-        std::vector<float> nmsConfidences;
-        std::vector<int> nmsClassIds;
-        for (std::map<int, std::vector<size_t> >::iterator it = class2indices.begin(); it != class2indices.end(); ++it)
-        {
-            std::vector<Rect> localBoxes;
-            std::vector<float> localConfidences;
-            std::vector<size_t> classIndices = it->second;
-            for (size_t i = 0; i < classIndices.size(); i++)
-            {
-                localBoxes.push_back(boxes[classIndices[i]]);
-                localConfidences.push_back(confidences[classIndices[i]]);
-            }
-            std::vector<int> nmsIndices;
-            NMSBoxes(localBoxes, localConfidences, confThreshold, nmsThreshold, nmsIndices);
-            for (size_t i = 0; i < nmsIndices.size(); i++)
-            {
-                size_t idx = nmsIndices[i];
-                nmsBoxes.push_back(localBoxes[idx]);
-                nmsConfidences.push_back(localConfidences[idx]);
-                nmsClassIds.push_back(it->first);
-            }
-        }
-        boxes = nmsBoxes;
-        classIds = nmsClassIds;
-        confidences = nmsConfidences;
-    }
-
+    printf("Confidence size is %d\n", (int)confidences.size());
+    
     for (size_t idx = 0; idx < boxes.size(); ++idx)
     {
         Rect box = boxes[idx];
@@ -438,4 +383,18 @@ void drawPred(int classId, float conf, int left, int top, int right, int bottom,
 void callback(int pos, void*)
 {
     confThreshold = pos * 0.01f;
+}
+
+bool AddPoint(const Point& point, const std::vector<Point>& points)
+{
+    if (points.empty())
+        return true;
+    for(size_t j=0; j<points.size(); j++){
+        int diff_x = point.x - points[j].x;
+        int diff_y = point.y - points[j].y;
+        printf("%d %d\n", diff_x, diff_y);
+        if (diff_x*diff_x + diff_y*diff_y > 10000)
+            return false;
+    }
+    return false;
 }
